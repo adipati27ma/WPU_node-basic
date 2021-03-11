8132967ea7fb3422a8075136ebba3c34dabaee92
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-11 18:13:05 +0700

    initial commit

---
+++ coba2.js
@@ -0,0 +1,8 @@
+const nama = "Adipati";
+const umur = 22;
+
+const sayHello = (nama, umur) => {
+  console.log(`Hello ${nama}, kamu berumur ${umur}`);
+};
+
+sayHello(nama, umur);
---
+++ practiceCoreModule.js
@@ -0,0 +1,17 @@
+// Core Module
+// File System
+const fs = require("fs");
+
+//! menuliskan string ke file (sync)
+// try {
+//   fs.writeFileSync("data/test.txt", "Hello world sync!");
+// } catch (err) {
+//   console.log(err);
+// }
+
+//! menuliskan string ke file (async)
+// fs.writeFile("datae/text2.txt", "Hello world async!", (err) => {
+//   console.log(err);
+// });
+
+//! membaca file string (sync)
